5aa0ad6ea7ca8056414968587ae66578dcc47006
Sergey Mushkaev <mushkaev@module.ru>
2022-04-04 15:42:57 +0300

    1234

--- examples/premake5.lua
+++ examples/premake5.lua
@@ -68,6 +68,22 @@ solution "vshell-examples"
 	configuration "Release"
          defines { "NDEBUG" }
          symbols  "Off" 
+	
+	--configurations { "Debug", "Release" }
+	project "SignalPlot"
+    kind "ConsoleApp"
+    files {"SignalPlot.cpp"}
+	characterset ("MBCS") --  Multi-byte Character Set; currently Visual Studio only
+	includedirs { "../include","$(SIGNALPLOT64)/include"}
+	libdirs { "../lib","$(SIGNALPLOT64)/lib"} 
+	links { "vshell.lib","signalplot64.lib" }
+	debugenvs { "PATH=%PATH%;$(ProjectDir)/../bin" }
+	configuration "Debug"
+         defines { "DEBUG" }
+         symbols  "On" 
+	configuration "Release"
+         defines { "NDEBUG" }
+         symbols  "Off" 
 		
 	 
  	
